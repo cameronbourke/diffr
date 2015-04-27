89f372c018a6466d12ea508125e4e76e82b8f9b4
cameronbourke <cameron2295@gmail.com>
2015-04-27 12:32:32 +1000

    add ngModelOptions
    Signed-off-by: cameronbourke <cameron2295@gmail.com>

--- src/js/ctrls/search.controller.js
+++ src/js/ctrls/search.controller.js
@@ -9,19 +9,33 @@
     var search = this;
 
     // variables
+    search.modelOptions = {
+      updateOn: 'default blur',
+      debounce: {
+        'default': 1 * 1000,
+        'blur': 0
+      }
+    };
+    search.searchedText = 'seflie';
 
     // methods
     search.getSearchedPhotos = getSearchedPhotos;
+    search.updatePhotos      = updatePhotos;
 
     // initiators
-    getSearchedPhotos('dogs');
+    getSearchedPhotos('selfie');
 
 
     function getSearchedPhotos(text) {
       FlickrService.getSearchedPhotos(text).then(function(photos) {
         console.log(photos);
         search.searchedPhotosList = photos;
+        search.searchedText = text;
       });
+    }
+
+    function updatePhotos(newValue) {
+      getSearchedPhotos(newValue);
     }
 
   }
